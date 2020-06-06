9db29d9c396b8b7a0624a0cef3cec74fd1ab070e
Jesus Bohorquez <bohorquez866@gmail.com>
2020-06-05 21:27:27 -0500

    finish show card

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,26 @@ const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    weather = data.weather;
+    //update Details Template
+
+    details.innerHTML =
+        `<h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>`;
+    //remove d-none
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+
+}
+
+
 
 const updateCity = async city => {
     const cityDets = await getCity(city);
@@ -19,6 +39,6 @@ cityForm.addEventListener('submit', e => {
 
     //update UI with new Info
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(error => console.log(error))
 })
