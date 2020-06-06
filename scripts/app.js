3a5cc33959ca4ab1d2c0ca411eda6045293e0dc6
Jesus Bohorquez <bohorquez866@gmail.com>
2020-06-05 22:37:34 -0500

    Weather App Complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,17 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon > img')
+
+console.log(icon);
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    weather = data.weather;
-    //update Details Template
+    //// const cityDets = data.cityDets;
+    //// const weather = data.weather;
 
+    const { cityDets, weather } = data
+    //update Details Template
     details.innerHTML =
         `<h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
@@ -14,7 +19,18 @@ const updateUI = (data) => {
             <span>${weather.Temperature.Metric.Value}</span>
             <span>&deg;C</span>
         </div>`;
-    //remove d-none
+
+    //Update night/day icon & imgs
+
+    let timeSrc = weather.IsDayTime ? timeSrc = './img/day.svg' : timeSrc = './img/night.svg';
+    const iconSrc = `./img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // if (weather.IsDayTime) { timeSrc = './img/day.svg'; } else { timeSrc = './img/night.svg'; }
+
+    time.setAttribute('src', timeSrc);
+
+    //remove d-none (if present)
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
