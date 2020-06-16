2041a7ffd07f0bb513f9fb1a41de90e63dd7feeb
Jesus Bohorquez <bohorquez866@gmail.com>
2020-06-16 03:22:12 -0500

    wheater app with OOP

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,31 +1,39 @@
-const key = "lXQGKxUqJ3WCLvZykDpAtFPS9alSK72h";
+class Forecast {
+    constructor() {
+        this.key = "lXQGKxUqJ3WCLvZykDpAtFPS9alSK72h";
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return { cityDets, weather }
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        if (response.status !== 200) {
+            throw new error('cannot fetch data')
+        }
+        return data[0];
+    }
+    async getWeather(id) {
 
-
-//Get current conditions
-const getWeather = async(id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
 }
 
-//Get city information
-const getCity = async city => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-    if (response.status !== 200) {
-        throw new error('cannot fetch data')
-    }
-    return data[0];
-
-};
 
 
 
-getCity('manchester').then(data => {
-        return getWeather(data.Key)
-    }).then(data => console.log(data))
-    .catch(error => console.log('Rejected', error));
+
+
+
+// getCity('manchester').then(data => {
+//         return getWeather(data.Key)
+//     }).then(data => console.log(data))
+//     .catch(error => console.log('Rejected', error));
